40c7b35bab7c43209deba1c197c6083cfae67bc0
Adam Moss <adamrmoss@me.com>
2015-03-21 19:08:09 -0400

    wip

--- source/kernel.asm
+++ source/kernel.asm
@@ -1,12 +1,15 @@
 bits 32
 
+    TEXT_COLS    equ 80
+    TEXT_ROWS    equ 80
+
 org 0x00100000
 code:
   .multiboot:
     MBALIGN      equ  1<<0
     MEMINFO      equ  1<<1
     AOUT_KLUDGE  equ  1<<16
-    
+
     FLAGS        equ  MBALIGN | MEMINFO | AOUT_KLUDGE
     MAGIC        equ  0x1badb002
     CHECKSUM     equ -(MAGIC + FLAGS)
@@ -20,6 +23,15 @@ code:
     dd stack.end
     dd code.entry
   .entry:
+    mov ebx, 0xb8000
+    xor eax, eax
+  .clsLoop:
+    mov ecx, TEXT_COLS * TEXT_ROWS / 2
+    mov dword [ebx], eax
+    add ebx, 4
+    loop .clsLoop
+
+  .showGreeting:
     mov edi, 0xb8000
     mov esi, data.greeting
     mov ah, 0x0f
@@ -31,13 +43,14 @@ code:
     hlt
     jmp short $
     align 4
+ .end:
 
 data:
   .greeting:
     db "Welcome to mos", 0
-  .end
-
     align 4
+ .end:
+
 stack:
     times 0x1000 dd 0
-  .end
+  .end:
