fe814cd0d14f20515f564b4a86a2d8124b629d21
Adam Moss <adamrmoss@me.com>
2015-03-21 21:18:35 -0400

    wip

--- source/kernel.asm
+++ source/kernel.asm
@@ -1,7 +1,26 @@
 bits 32
+    VIDEO_BASE equ 0xb8000
+    TEXT_COLS  equ 80
+    TEXT_ROWS  equ 25
 
-    TEXT_COLS    equ 80
-    TEXT_ROWS    equ 80
+    BLACK    equ 0x0
+    DBLUE    equ 0x1
+    DGREEN   equ 0x2
+    DCYAN    equ 0x3
+    DRED     equ 0x4
+    DMAGENTA equ 0x5
+    BROWN    equ 0x6
+    LGREY    equ 0x7
+    DGREY    equ 0x8
+    LBLUE    equ 0x9
+    LGREEN   equ 0xa
+    LCYAN    equ 0xb
+    LRED     equ 0xc
+    LMAGENTA equ 0xd
+    YELLOW   equ 0xe
+    WHITE    equ 0xf
+
+    THEME equ DBLUE << 4 | LGREEN
 
 org 0x00100000
 code:
@@ -10,9 +29,10 @@ code:
     MEMINFO      equ  1<<1
     AOUT_KLUDGE  equ  1<<16
 
-    FLAGS        equ  MBALIGN | MEMINFO | AOUT_KLUDGE
-    MAGIC        equ  0x1badb002
-    CHECKSUM     equ -(MAGIC + FLAGS)
+    FLAGS     equ  MBALIGN | MEMINFO | AOUT_KLUDGE
+    MAGIC     equ  0x1badb002
+    CHECKSUM  equ -(MAGIC + FLAGS)
+
     align 4
     dd MAGIC
     dd FLAGS
@@ -22,9 +42,12 @@ code:
     dd data.end
     dd stack.end
     dd code.entry
+
   .entry:
-    mov ebx, 0xb8000
-    mov eax, 0x1f001f00
+  .cls:
+    mov esp, stack.end
+    mov ebx, VIDEO_BASE
+    mov eax, THEME << 24 | THEME << 8
   .clsLoop:
     mov ecx, TEXT_COLS * TEXT_ROWS / 2
     mov dword [ebx], eax
@@ -32,9 +55,10 @@ code:
     loop .clsLoop
 
   .showGreeting:
-    mov edi, 0xb8000
+    GREETING_OFFSET equ TEXT_ROWS * TEXT_COLS - (data.greetingEnd - data.greeting - 1)
+    mov edi, VIDEO_BASE + GREETING_OFFSET
     mov esi, data.greeting
-    mov ah, 0x1f
+    mov ah, THEME
   .charLoop:
     lodsb
     stosw
@@ -47,7 +71,8 @@ code:
 
 data:
   .greeting:
-    db "Welcome to mos", 0
+    db "welcome to mos", 0
+  .greetingEnd:
     align 4
  .end:
 
